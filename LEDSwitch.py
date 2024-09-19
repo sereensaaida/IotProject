a627d3209b4464cda7c135bb58789de87fa34b6c
jaydapizzuco <jaydapizzuco@gmail.com>
2024-09-19 18:57:53 -0400

    clicking light switch on the dashboard now controls LED, using Flask

--- LEDSwitch.py
+++ LEDSwitch.py
@@ -1,21 +1,35 @@
-from flask import Flask, jsonify, request
+from flask import Flask,send_from_directory,request
 import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
+# set GPIO pin 18 to output
 GPIO.setmode(GPIO.BCM)
 LED= 18
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(LED, GPIO.OUT)
 
-@app.route('/turnon', methods=['POST'])
+#set the root as the html file, put html file on Flask's server
+@app.route('/')
+def index():
+    return send_from_directory('.','main.html')
+
+#serve the image files
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.',filename)
+
+#if the value received through post is turn_on, turn the LED on
+@app.route('/turn_on', methods=['POST'])
 def turnOn():
     GPIO.output(LED,GPIO.HIGH)
     return "ON"
 
-@app.route('/turnoff', methods=['POST'])
+
+#if the value received through post is turn_off, turn the LED off
+@app.route('/turn_off', methods=['POST'])
 def turnOff():
-    GPIO.output(LED,GPIO.HIGH)
+    GPIO.output(LED,GPIO.LOW)
     return "OFF"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5001,debug=True,)
