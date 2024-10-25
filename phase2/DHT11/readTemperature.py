de7ff5daf91078211bc4904e08d5b04803ca5b81
jaydapizzuco <jaydapizzuco@gmail.com>
2024-10-24 20:37:15 -0400

    added javascript code to call python methods on Flask app that check/retrieve temperature and humidity and send emails, temp and humidity are now returned as json array and displayed on dashboard as text

--- phase2/DHT11/readTemperature.py
+++ phase2/DHT11/readTemperature.py
@@ -1,4 +1,4 @@
-from flask import Flask,send_from_directory,request
+from flask import Flask,send_from_directory,request,jsonify
 import time
 from Freenove_DHT import DHT
 import os
@@ -26,7 +26,7 @@ def read_temperature_and_humidity():
 # function to send the user an email if the temperature is more than 24 degrees celsius
 def send_email(temperature):
     to=["jpizzuco22@gmail.com"]
-    message="The current temperature is {}°C. Would you like to turn on the fan?"%(temperature)
+    message="The current temperature is %.2f °C. Would you like to turn on the fan?"%(temperature)
         
     email_id='testingthis2283@gmail.com'
     email_pass='gsyx yxmi rnaq lwud'
@@ -55,13 +55,13 @@ def serve_static(filename):
 #check temperature and send email if temperature is greater than 24 degrees celsius
 @app.route('/checktemperature')
 def check_temperature():
-    temperature_humidity = read_temperature_and_humidity();
+    temperature,humidity = read_temperature_and_humidity();
     
     if temperature is not None:
         if temperature > 24:
             send_email(temperature)
             
-        return ""%(temperature,humidity)
+        return jsonify({'temperature':temperature,'humidity': humidity})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001,debug=True,)
