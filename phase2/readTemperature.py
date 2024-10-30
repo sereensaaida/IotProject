35675abfb36fcc3c8b1f7152ca2db44c1fb25c19
overcookedcircuit <manaspatel31122004@gmail.com>
2024-10-30 14:51:24 -0400

    added motor.py and easier config for dht11

--- phase2/readTemperature.py
+++ phase2/readTemperature.py
@@ -1,4 +1,4 @@
-from flask import Flask,send_from_directory,request
+from flask import Flask,send_from_directory,request,jsonify
 import time
 from Freenove_DHT import DHT
 import os
@@ -7,7 +7,6 @@ from email.message import EmailMessage
 
 
 app = Flask(__name__)
-
 
 DHTPin = 17     #define the pin of DHT11
 
@@ -27,7 +26,7 @@ def read_temperature_and_humidity():
 # function to send the user an email if the temperature is more than 24 degrees celsius
 def send_email(temperature):
     to=["jpizzuco22@gmail.com"]
-    message="The current temperature is {}°C. Would you like to turn on the fan?"%(temperature)
+    message="The current temperature is %.2f °C. Would you like to turn on the fan?"%(temperature)
         
     email_id='testingthis2283@gmail.com'
     email_pass='gsyx yxmi rnaq lwud'
@@ -46,23 +45,23 @@ def send_email(temperature):
 #set the root as the html file, put html file on Flask's server
 @app.route('/')
 def index():
-    return send_from_directory('.','main.html')
+    return send_from_directory('../','main.html')
 
 #serve the image files
 @app.route('/<path:filename>')
 def serve_static(filename):
-    return send_from_directory('.',filename)
+    return send_from_directory('../',filename)
 
 #check temperature and send email if temperature is greater than 24 degrees celsius
 @app.route('/checktemperature')
 def check_temperature():
-    temperature_humidity = read_temperature_and_humidity();
+    temperature,humidity = read_temperature_and_humidity();
     
     if temperature is not None:
         if temperature > 24:
             send_email(temperature)
             
-        return ""%(temperature,humidity)
+        return jsonify({'temperature':temperature,'humidity': humidity})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001,debug=True,)
