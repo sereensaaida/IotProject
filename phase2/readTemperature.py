3cfc59d009596a0cec1eb30bf3f7d87ae33559f0
overcookedcircuit <manaspatel31122004@gmail.com>
2024-11-03 16:54:47 -0500

    changed fan btn for JSON

--- phase2/readTemperature.py
+++ phase2/readTemperature.py
@@ -1,4 +1,4 @@
-from flask import Flask,send_from_directory,request,jsonify
+from flask import Flask, send_from_directory, request, jsonify
 import time
 from Freenove_DHT import DHT
 import os
@@ -6,50 +6,47 @@ import smtplib
 import email
 import imaplib
 from email.message import EmailMessage
-from motor import turn_on_fan
-
+from motor import turn_on_fan  # Assuming `motor.py` has the function to control fan hardware
 
 app = Flask(__name__)
 
-DHTPin = 17     #define the pin of DHT11
+DHTPin = 17  # Define the pin of DHT11
 latest_temperature = None
 
-# function to read temperature and humidity using DHT11
+# Function to read temperature and humidity using DHT11
 def read_temperature_and_humidity():
     dht = DHT(DHTPin)
     time.sleep(1)
-    for i in range(0,15):            
-            chk = dht.readDHT11()     #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-            if (chk == 0):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-                temperature= dht.getTemperature()
-                humidity = dht.getHumidity()
-                return temperature,humidity
-            time.sleep(0.1)
-    return None,None
+    for i in range(15):
+        chk = dht.readDHT11()  # Read DHT11 and check if data read is normal
+        if chk == 0:
+            temperature = dht.getTemperature()
+            humidity = dht.getHumidity()
+            return temperature, humidity
+        time.sleep(0.1)
+    return None, None
 
-# function to send the user an email if the temperature is more than 24 degrees celsius
+# Function to send an email if temperature is more than 24°C
 def send_email(temperature):
-    to=["iotclient26@gmail.com"]
-    message="The current temperature is %.2f °C. Would you like to turn on the fan?"%(temperature)
-        
-    email_id='testingthis2283@gmail.com'
-    email_pass='gsyx yxmi rnaq lwud'
+    to = ["iotclient26@gmail.com"]
+    message = f"The current temperature is {temperature:.2f} °C. Would you like to turn on the fan?"
 
-    msg=EmailMessage()
-    msg['Subject']='Sending Email'
-    msg['From']=email_id
-    msg['To']= to
+    email_id = 'testingthis2283@gmail.com'
+    email_pass = 'gsyx yxmi rnaq lwud'
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Temperature Alert'
+    msg['From'] = email_id
+    msg['To'] = to
     msg.set_content(message)
 
-    with smtplib.SMTP_SSL('smtp.gmail.com',465)as smtp:
-            smtp.login(email_id,email_pass)
-            smtp.send_message(msg)
-            smtp.quit()
-            
-    print('email sent')
-            
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_id, email_pass)
+        smtp.send_message(msg)
         
-#function to read email, checks latest email in inbox for 'yes'
+    print('Email sent asking if the fan should be turned on.')
+
+# Function to read the latest email and check if it contains "yes"
 def check_email_for_yes():
     email_id = 'testingthis2283@gmail.com'
     password = 'gsyx yxmi rnaq lwud'
@@ -59,78 +56,83 @@ def check_email_for_yes():
     mail.login(email_id, password)
     mail.select('inbox')
 
-    #search for unseen emails
-    status, data = mail.search(None,'(UNSEEN)')
+    # Search for unseen emails
+    status, data = mail.search(None, '(UNSEEN)')
     mail_ids = data[0].split()
 
-    #select last email in inbox
-    last_email_in_inbox = mail_ids[-1]
-    status, data = mail.fetch(last_email_in_inbox, '(RFC822)')
+    if not mail_ids:
+        return False  # No new emails
+
+    # Select the most recent email
+    last_email_id = mail_ids[-1]
+    status, data = mail.fetch(last_email_id, '(RFC822)')
     raw_email = data[0][1]
 
-    #parse email into a message object so we can read it
+    # Parse email to get content
     message = email.message_from_bytes(raw_email)
-    mail_from = message['from']
-    mail_subject = message['subject']
-
+    mail_content = ''
     if message.is_multipart():
-                    mail_content = ''
+        for part in message.get_payload():
+            if part.get_content_type() == 'text/plain':
+                mail_content += part.get_payload()
+    else:
+        mail_content = message.get_payload()
 
-    for part in message.get_payload():
-                if part.get_content_type() == 'text/plain':
-                            mail_content += part.get_payload()
-                            
-    print(f'From: {mail_from}')
-    print(f'Subject: {mail_subject}')
-    print(f'Content: {mail_content}')
-    
     mail_content = mail_content.lower()
-    
-    if 'yes' in mail_content:
-        turn_on_fan()
-        return True
-    
+    print(f'Content of latest email: {mail_content}')
 
-    
-#set the root as the html file, put html file on Flask's server
+    # Check if "yes" is in the content to turn on the fan
+    if 'yes' in mail_content:
+        turn_on_fan()  # Turn on fan
+        return True
+
+    return False
+
+# Serve the main HTML file
 @app.route('/')
 def index():
-    return send_from_directory('.','main.html')
+    return send_from_directory('.', 'main.html')
 
-#serve the image files
+# Serve static files like images, CSS, JavaScript
 @app.route('/<path:filename>')
 def serve_static(filename):
-    return send_from_directory('.',filename)
+    return send_from_directory('.', filename)
 
-#check temperature and send it to the dahsboard
+# Endpoint to check temperature and humidity
 @app.route('/checktemperature')
 def check_temperature():
     global latest_temperature
-    temperature,humidity = read_temperature_and_humidity();
-    
+    temperature, humidity = read_temperature_and_humidity()
+
     if temperature is not None:
         latest_temperature = temperature
-        return jsonify({'temperature':temperature,'humidity': humidity,'fan_status': 'off'})
+        return jsonify({
+            'temperature': temperature,
+            'humidity': humidity,
+            'fan_status': 'off'
+        })
 
-#send email if temperature is greater than 24 degrees celsius and check for  response to the email
-#if a response to the email has been found then tell the dashboard that the fan has been turned on
+# Endpoint to send email and check for response to turn on fan
 @app.route('/sendemail')
 def send_email_check_for_response():
-    print(latest_temperature)
-    send_email(latest_temperature)
-            
-    #wait 60 seconds before checking inbox
-    time.sleep(60)
-            
-    for i in range(10):
-        email_response = check_email_for_yes()
-        #method only returns True if yes has been found in the email
-        if email_response:
-              #response is json array containing fan status that we need for the main.html
-              return  jsonify({'fan_status': 'on'})
-                
-    time.sleep(10) #check again in 10 seconds
-                
+    global latest_temperature
+    
+    # Send an email if temperature is above the threshold
+    if latest_temperature is not None and latest_temperature > 24:
+        send_email(latest_temperature)
+        
+        # Wait a bit before checking for a response
+        time.sleep(60)
+
+        # Check for a "yes" response in email periodically
+        for _ in range(10):  # Check 10 times at 10-second intervals
+            if check_email_for_yes():
+                # Return fan status as "on" if "yes" was received
+                return jsonify({'fan_status': 'on'})
+            time.sleep(10)
+
+    # Return fan status as "off" if no "yes" response was found
+    return jsonify({'fan_status': 'off'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001,debug=True,)
+    app.run(host='0.0.0.0', port=5001, debug=True)
