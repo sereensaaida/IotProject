bc61de10da345ea0d3256ba1a61977d99c53d8ec
overcookedcircuit <manaspatel31122004@gmail.com>
2024-11-26 02:22:14 -0500

    motor code update

--- Phase4/motor.py
+++ Phase4/motor.py
@@ -1,31 +1,39 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-def turn_on_fan():
-    print('fan turning on')
+# Pin configuration
+Motor1 = 21  # Enable Pin
+Motor2 = 12  # Input Pin
+Motor3 = 25  # Input Pin
+
+def setup_gpio():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
+    GPIO.setup(Motor1, GPIO.OUT)
+    GPIO.setup(Motor2, GPIO.OUT)
+    GPIO.setup(Motor3, GPIO.OUT)
 
-    Motor1 = 21 # Enable Pin
-    Motor2 = 12 # Input Pin
-    Motor3 = 25 # Input Pin
+def turn_on_fan():
+    print('Fan turning on')
+    setup_gpio()
+    GPIO.output(Motor1, GPIO.HIGH)
+    GPIO.output(Motor2, GPIO.LOW)
+    GPIO.output(Motor3, GPIO.HIGH)
+    print('Fan is running...')
 
-    GPIO.setup(Motor1,GPIO.OUT)
-    GPIO.setup(Motor2,GPIO.OUT)
-    GPIO.setup(Motor3,GPIO.OUT)
-
-    GPIO.output(Motor1,GPIO.HIGH)
-    GPIO.output(Motor2,GPIO.LOW)
-    GPIO.output(Motor3,GPIO.HIGH)
-    sleep(5)
-
-    GPIO.output(Motor1,GPIO.HIGH)
-    GPIO.output(Motor2,GPIO.HIGH)
-    GPIO.output(Motor3,GPIO.LOW)
-    sleep(5)
-
-    GPIO.output(Motor1,GPIO.LOW)
+def turn_off_fan():
+    print('Fan turning off')
+    GPIO.output(Motor1, GPIO.LOW)
+    GPIO.output(Motor2, GPIO.LOW)
+    GPIO.output(Motor3, GPIO.LOW)
     GPIO.cleanup()
+    print('Fan stopped')
 
 if __name__ == "__main__":
-    turn_on_fan()
+    try:
+        turn_on_fan()
+        # Keep running until interrupted
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        turn_off_fan()
